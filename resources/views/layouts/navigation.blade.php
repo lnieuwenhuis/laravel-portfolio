d5de40e38520e3ae0b40fdba20efbd46a9340609
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-03-04 20:58:46 +0100

    responsive navigation for admin projects added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -81,6 +81,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
+                {{ __('Projects') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
